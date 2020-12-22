83157df43e265ddfe65004923daa2a82c13a3a02
DanJSG <jackson.daniel365@gmail.com>
2020-12-22 15:12:40 +0000

    Added CSV presets for reverb

--- assignment.m
+++ assignment.m
@@ -5,6 +5,8 @@ note_2 = [zeros(1, Fs * 2), flute(Fs, 311.13, 2, 0.1, 0.3, 0.00001, 0.04)];
 note_3 = [zeros(1, Fs * 3), flute(Fs, 392.00, 1, 0.15, 0.3, 0.00001, 0.06)];
 note_4 = flute(Fs, 233.08, 4, 0.2, 0.3, 0.00001, 0.1);
 
+reverbVals = readmatrix("./presets/reverb/YorkMinster.csv");
+
 combined = 0.2 * (note_1 + note_2 + note_3 + note_4);
 
 % [drums, Fs] = audioread("drums.wav");
